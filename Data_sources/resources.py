6dbda9f69f50a05802522f0cf8f627e485498094
Andrei Monichev <75957286+Vyroum@users.noreply.github.com>
2024-12-11 18:47:04 +0300

    Delete Data_sources directory

--- Data_sources/resources.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-import tests_for_practice_form
-
-
-def path(file_name):
-    return str(
-        Path(tests_for_practice_form.__file__).parent.joinpath(f'resources/{file_name}').absolute()
-    )
